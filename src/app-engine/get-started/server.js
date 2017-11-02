8a2dd314b6d1de65078bfd010e88a6e6abc2d4da
mrdulin <novaline@qq.com>
2017-11-02 10:37:56 +0800

    add app-engine/get-started sample

---
+++ src/app-engine/get-started/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello from App Engine!');
+});
+
+app.get('/submit', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/form.html'));
+});
+
+app.post('/submit', (req, res) => {
+  console.log({
+    name: req.body.name,
+    message: req.body.message
+  });
+  res.send('Thanks for your message!');
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
