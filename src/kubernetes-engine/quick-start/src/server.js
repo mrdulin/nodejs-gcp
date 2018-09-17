175d47b7a4ed6dd6184876504cf26b5ebc85c288
mrdulin <novaline@qq.com>
2018-09-17 18:11:08 +0800

    improve k8s sample

--- src/kubernetes-engine/quick-start/src/server.js
+++ src/kubernetes-engine/quick-start/src/server.js
@@ -13,6 +13,11 @@ app.get('/', (req, res) => {
   res.send(`version:${package.version}`);
 });
 
+app.post('/webhook/v1/facebook', (req, res) => {
+  console.log('webhook works');
+  res.sendStatus(200);
+});
+
 app.listen(config.PORT, () => {
   console.log(`Server listening on http://localhost:${config.PORT}`);
 });
