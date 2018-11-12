27fa9c420e7708cb080193cf8f2f89c6b07f8e48
mrdulin <novaline@qq.com>
2018-11-12 11:05:11 +0800

    update src/app-engine/standard-environment/cron-jobs

--- src/app-engine/standard-environment/cron-jobs/server.js
+++ src/app-engine/standard-environment/cron-jobs/server.js
@@ -1,19 +1,33 @@
 const express = require('express');
+const pkg = require('./package.json');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('GAE Cron Service test');
-});
-
-app.get('/tasks/sync', (req, res) => {
+function validateCronRequest(req, res, next) {
   console.log('X-Appengine-Cron', req.get('X-Appengine-Cron'), typeof req.get('X-Appengine-Cron'));
   if (req.get('X-Appengine-Cron') !== 'true') {
     return res.status(403);
   }
+  next();
+}
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get('/version', (req, res) => {
+  res.send(`version: ${pkg.version}`);
+});
+
+app.get('/tasks/sync', validateCronRequest, (req, res) => {
   console.log('Doing sync task');
-  res.send('Doing sync task');
-})
+  res.sendStatus(200);
+});
+
+app.get('/cron/events/:topic/:retryTopic', validateCronRequest, (req, res) => {
+  console.log(`req.params: ${JSON.stringify(req.params)}`);
+  res.sendStatus(200);
+});
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
