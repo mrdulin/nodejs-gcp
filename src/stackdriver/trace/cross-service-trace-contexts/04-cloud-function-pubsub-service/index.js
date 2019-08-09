cdd80661d88e0c17061b34e4aa125fb151552311
mrdulin <novaline@qq.com>
2019-08-09 20:30:24 +0800

    improve sample

--- src/stackdriver/trace/cross-service-trace-contexts/04-cloud-function-pubsub-service/index.js
+++ src/stackdriver/trace/cross-service-trace-contexts/04-cloud-function-pubsub-service/index.js
@@ -7,35 +7,95 @@ const fetch = require('node-fetch');
 const CLOUD_FUNCTION_HTTP_SERVICE_URL = process.env.CLOUD_FUNCTION_HTTP_SERVICE_URL || '';
 console.log('CLOUD_FUNCTION_HTTP_SERVICE_URL: ', CLOUD_FUNCTION_HTTP_SERVICE_URL);
 
-exports.TestCloudTracePubSubService = function TestCloudTrace(data, context, callback) {
+// exports.TestCloudTracePubSubService = function TestCloudTrace(data, context, callback) {
+//   const pubSubMessage = data;
+//   const jsonString = pubSubMessage.data ? Buffer.from(pubSubMessage.data, 'base64').toString() : '{}';
+//   const message = JSON.parse(jsonString);
+//   console.log(`message: ${JSON.stringify(message, null, 2)}`);
+//   const traceContext = parseContextFromHeader(message.traceContext);
+
+//   tracer.runInRootSpan(
+//     {
+//       name: 'TestCloudTracePubSubService', // Your function name here
+//       traceContext
+//     },
+//     async (rootSpan) => {
+//       // Your logic here. Just be sure to call endSpan() after sending a response.
+//       const fetchChildSpan = tracer.createChildSpan({ name: 'fetch data from http cloud function' });
+//       const fetchChildSpanTraceContext = fetchChildSpan.getTraceContext();
+//       console.log('fetchChildSpanTraceContext: ', fetchChildSpanTraceContext);
+//       const body = await fetch(CLOUD_FUNCTION_HTTP_SERVICE_URL, {
+//         headers: {
+//           // [tracer.constants.TRACE_CONTEXT_HEADER_NAME]: message.traceContext
+//           'x-cloud-trace-context': message.traceContext
+//         }
+//       }).then((res) => res.json());
+//       console.log(`body: ${JSON.stringify(body, null, 2)}`);
+//       fetchChildSpan.endSpan();
+//       callback();
+//     }
+//   );
+// };
+
+async function TestCloudTrace(data, context, callback) {
+  console.log('test trace enhance');
   const pubSubMessage = data;
   const jsonString = pubSubMessage.data ? Buffer.from(pubSubMessage.data, 'base64').toString() : '{}';
   const message = JSON.parse(jsonString);
-  console.log(`message: ${JSON.stringify(message, null, 2)}`);
-  const traceContext = parseContextFromHeader(message.traceContext);
-  tracer.runInRootSpan(
-    {
-      name: 'TestCloudTracePubSubService', // Your function name here
-      traceContext
-    },
-    async (rootSpan) => {
-      // Your logic here. Just be sure to call endSpan() after sending a response.
-      const fetchChildSpan = tracer.createChildSpan({ name: 'fetch data from http cloud function' });
-      const fetchChildSpanTraceContext = fetchChildSpan.getTraceContext();
-      console.log('fetchChildSpanTraceContext: ', fetchChildSpanTraceContext);
-      const body = await fetch(CLOUD_FUNCTION_HTTP_SERVICE_URL, {
-        headers: {
-          // [tracer.constants.TRACE_CONTEXT_HEADER_NAME]: message.traceContext
-          'x-cloud-trace-context': message.traceContext
-        }
-      }).then((res) => res.json());
-      console.log(`body: ${JSON.stringify(body, null, 2)}`);
-      fetchChildSpan.endSpan();
-      rootSpan.endSpan();
-      callback();
+  const traceContext = generateTraceContext(message.traceContext);
+  console.log('traceContext: ', traceContext);
+  const body = await fetch(CLOUD_FUNCTION_HTTP_SERVICE_URL, {
+    headers: {
+      'x-cloud-trace-context': traceContext
     }
-  );
-};
+  }).then((res) => res.json());
+  console.log(`body: ${JSON.stringify(body, null, 2)}`);
+  callback();
+}
+
+exports.TestCloudTracePubSubService = traceEnhance(TestCloudTrace);
+
+function traceEnhance(cloudfunction) {
+  return function originalCloudFunction(data, context, callback) {
+    const pubSubMessage = data;
+    const jsonString = pubSubMessage.data ? Buffer.from(pubSubMessage.data, 'base64').toString() : '{}';
+    const message = JSON.parse(jsonString);
+    console.log(`message: ${JSON.stringify(message, null, 2)}`);
+    // const traceContext = parseContextFromHeader(message.traceContext);
+
+    tracer.runInRootSpan(
+      {
+        name: cloudfunction.name,
+        traceContext: message.traceContext
+      },
+      (rootSpan) => {
+        // const span = tracer.createChildSpan({ name: 'Function execution' });
+        rootSpan.addLabel('message', message);
+        rootSpan.addLabel('context', context);
+        rootSpan.addLabel('traceContext', message.traceContext);
+        function wrappedCallback(...args) {
+          // span.endSpan();
+          rootSpan.endSpan();
+          console.log('trace root span end');
+          callback.apply(this, args);
+        }
+
+        cloudfunction.call(this, data, context, wrappedCallback);
+      }
+    );
+  };
+}
+
+function generateTraceContext(traceContext) {
+  if (!traceContext) {
+    return '';
+  }
+  let header = `${traceContext.traceId}/${traceContext.spanId}`;
+  if (typeof traceContext.options !== 'undefined') {
+    header += `;o=${traceContext.options}`;
+  }
+  return header;
+}
 
 function parseContextFromHeader(str) {
   if (!str) {
